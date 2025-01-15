9dcbf921237547e6754d8f2e742fccbd4b2d23df
Dark-LYNN <botdevlynn@gmailcom>
2025-01-15 21:43:32 +0100

    Added pathResolve function.

---
+++ dist/functions/file/pathResolve.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const forgescript_1 = require("@tryforge/forgescript");
+const path_1 = __importDefault(require("path"));
+exports.default = new forgescript_1.NativeFunction({
+    name: "$pathResolve",
+    version: "1.0.0",
+    output: forgescript_1.ArgType.String,
+    description: "Resolve paths into an absolute path",
+    unwrap: true,
+    args: [
+        {
+            name: "main",
+            description: "Main path to start resolving from",
+            required: true,
+            rest: false,
+            type: forgescript_1.ArgType.String,
+        },
+        {
+            name: "joins",
+            description: "Paths to resolve with main",
+            rest: true,
+            type: forgescript_1.ArgType.String,
+            required: true,
+        },
+    ],
+    brackets: true,
+    execute(ctx, [main, joins]) {
+        return this.success(path_1.default.resolve(main, ...joins));
+    },
+});
+//# sourceMappingURL=pathResolve.js.map
