877e1947296bda4d3e133932776ae32fd70cec4a
Dark-LYNN <botdevlynn@gmailcom>
2025-01-15 21:27:03 +0100

    Added pathJoin function.

---
+++ src/functions/file/pathJoin.ts
@@ -0,0 +1,30 @@
+import { ArgType, NativeFunction, Return } from "@tryforge/forgescript"
+import path from "path";
+
+export default new NativeFunction({
+  name: "$pathJoin",
+  version: "1.0.0",
+  output: ArgType.Boolean,
+  description: "Join paths together",
+  unwrap: true,
+  args: [
+    {
+      name: "main",
+      description: "Main path to start from",
+      required: true,
+      rest: false,
+      type: ArgType.String,
+    },
+    {
+      name: "joins",
+      description: "Path to join with main",
+      rest: true,
+      type: ArgType.String,
+      required: true,
+    },
+  ],
+  brackets: true,
+  execute(ctx, [main, matches]) {
+      return this.success(path.join(main, ...matches));
+  },
+})
